01d36364c0670253b18eacbf68e95bbde01d7ae3
amitkumar25011993 <amitkumar25011993@gmail.com>
2018-01-18 03:57:21 +0530

    first commit

---
+++ app/src/main/java/com/example/amit/swipeabletab/Fragment1.java
@@ -0,0 +1,20 @@
+package com.example.amit.swipeabletab;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by amit on 1/18/2018.
+ */
+
+public class Fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return  inflater.inflate(R.layout.frag1,container,false);
+    }
+}
